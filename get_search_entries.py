676bcefe44f33da1c0130107f0c6aee498f8923d
stoefelz <pascal@stoefelz.com>
2023-06-13 21:50:07 +0200

    now search returns json objects

--- get_search_entries.py
+++ get_search_entries.py
@@ -2,130 +2,130 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-global error
+# html.parser or lxml -> lxml needs pip3 lxml module
+html_parser = 'html.parser'
+# default image when no image is available
+default_image_url = 'https://www.stoefelz.com/no_image.svg'
 
-# returns the value of the first index in list, if not empty; should only have 1 index, but it doesnt matter
-
-def check_if_empty_return(string_list):
-    if len(string_list) != 0:
-        #\u200b fucks up the interpretation of the strings
-        if len(string_list) > 1000:
-            if string_list.find("onGdprBannerLoad"):
-                error = True
-                return "Error in String"
-        return string_list[0].text.lstrip().replace('\u200b', '')
+# returns the string, if not None
+# lstrip -> removes spaces and \n on left side of string
+# \u200b fucks up the interpretation of the strings
+def string_return_value(string):
+    if string is not None:
+        return string.text.lstrip().replace('\u200b', '')
     else:
-        return ""
+        return ''
 
-    # TODO für filter sollte sorting einfach in einen string umgewandelt werden, wo alle filter rein kommen, bsp: "/s/k0/reinland/kleidung"
-    # obwohl dann z.b. bei sorting zu beachten ist, dass "sortierung: blablabal" des is
-def get_search_entries(search_term, site = 1, sorting = "", seller = "", typ="", min_price = 0, max_price = -1):
+# returns array of search items
+def get_search_entries(search_term, **search_arguments):
     try:
-        # filter
-        category = "/k0"
-        if sorting != "preis":
-            sorting = ""
-        if seller != "privat" and seller != "gewerblich":
-            seller = ""
-        if typ != "angebote" and typ != "gesuche":
-            typ = ""
-        try:
-            if int(min_price) <= 0 or int(min_price) > 100000:
-                min_price = 0
-        except:
-            min_price = 0
-        try:
-            if int(max_price) < 0 or int(max_price) > 100000:
-                max_price = ""
-        except:
-            max_price = ""
+        city = search_arguments.get('city')
+        zip_code = search_arguments.get('zip_code')
+        zip_radius = search_arguments.get('zip_radius')
+        site_number = search_arguments.get('site_number')
+        sorting = search_arguments.get('sorting')
+        seller = search_arguments.get('seller')
+        typ = search_arguments.get('typ')
+        min_price = search_arguments.get('min_price')
+        max_price = search_arguments.get('max_price')
+  
+        # example url: 
+        # https://www.kleinanzeigen.de/s-/handy-telekom/sonstige/hessen/direktkaufen:aktiv/paketdienst:dhl/preis:10:800/oneplus/k0c173l4279r200+handy_telekom.art_s:sonstige+handy_telekom.condition_s:condition_new+handy_telekom.device_equipment_s:only_device
+        
+        # url compositor
+        url = 'https://www.kleinanzeigen.de/s-'
+        # TODO categories
+        category = 'k0'
 
-      # example for link: https://www.kleinanzeigen.de/s-sortierung:preis/anbieter:privat/anzeige:angebote/preis:1:10/k0
-        url = "https://www.kleinanzeigen.de/s/sortierung:" + sorting + "/anbieter:" + seller + "/anzeige:" + typ + "/preis:" + str(min_price) + ":" + str(max_price) + "/seite:" + str(site) + "/" + search_term + category
-        print(url)
+        if zip_code and city:
+            url += '/'+ city
+        if sorting:
+            url += '/sortierung:' + sorting
+        if seller:
+            url += '/anbieter:' + seller
+        if typ:
+            url += '/anzeige:' + typ 
+        if min_price and max_price:
+            url += '/preis:' + min_price + ':' + max_price 
+        elif min_price:
+            url += '/preis:' + min_price + ':' 
+        elif max_price:
+             url += '/preis:' + ':' + max_price 
+        if site_number:
+            url += '/seite:' + site_number
+        url += '/' + search_term
+        if category:
+            url += '/' + category
+        if zip_code and city:
+            url += 'l' + zip_code
+            if zip_radius:
+                url += zip_radius
 
-        default_image_url = "https://www.stoefelz.com/no_image.svg"
-
-            # without headers kleinanzeigen blocks request
-        headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0' }
+        # without headers kleinanzeigen blocks request
+        headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0' }
         html_site = requests.get(url, headers=headers)
-        soup = BeautifulSoup(html_site.text, "html.parser")
+        soup = BeautifulSoup(html_site.text, html_parser)
 
-            # only for testing, code above must be commented
-        # with open("suche2.html") as fp:
-         #  soup = BeautifulSoup(fp, "lxml",)
+        # only for testing, code above must be commented
+        # with open('../Downloads/fleisch.html') as fp:
+          # soup = BeautifulSoup(fp, html_parser)
 
         # list for return
-        list_with_data = []
-
-            # in every article tag is one complete search item
+        list_with_items = []
+        # in every article tag is one item
         articles = soup.find_all('article')
 
         for one_article in articles:
-            error = False
-            # for one search result
-            article_list = []
-
             # get id from attribute
             if one_article['data-adid'] is not None:
-                article_list.append(one_article['data-adid'])
+                item_id = one_article['data-adid']
             else:
-                print("no id found")
-                article_list.append("")
-
-            # get values from parsing correct class
-            heading = one_article.find_all("a", class_="ellipsis")
-            article_list.append(check_if_empty_return(heading))
-
-
-            text = one_article.find_all("p", class_="aditem-main--middle--description")
-            article_list.append(check_if_empty_return(text))
-
-            price = one_article.find_all("p", class_="aditem-main--middle--price-shipping--price")
-            article_list.append(check_if_empty_return(price))
-
-            zip_code = one_article.find_all("div", class_="aditem-main--top--left")
-            article_list.append(check_if_empty_return(zip_code))
-
-            create_date = one_article.find_all("div", class_="aditem-main--top--right")
-            article_list.append(check_if_empty_return(create_date))
-
-            # get picture url from class, but must check if there is a picture or not
-            picture = one_article.find_all("div", class_="imagebox")
-            if len(picture) != 0:
-
-                classes = picture[0]['class']
-                # classes returns string list of the class names
-                # searching for "is-nopic" then there is no image
-                checker = True
-
-                for one_class_name in classes:
-                    if one_class_name == "is-nopic":
-                        article_list.append(default_image_url)
-                        checker = False
-                        break
-
-                if checker == True:
-                    article_list.append(picture[0]['data-imgsrc'])
-
-            else:
-                article_list.append(default_image_url)
-
-            if error == True:
-                print("too long string")
                 continue
-            else:
-            # append it to the list for return
-                list_with_data.append(article_list)
+    
+            # heading
+            heading = one_article.find('a', class_='ellipsis')
+            heading = string_return_value(heading)
+            
+            # info
+            info_text = one_article.find('p', class_='aditem-main--middle--description')
+            info_text = string_return_value(info_text)
+            
+            # price
+            price = one_article.find('p', class_='aditem-main--middle--price-shipping--price')
+            price = string_return_value(price)
 
+            # zip code
+            zip_code = one_article.find('div', class_='aditem-main--top--left')
+            zip_code = string_return_value(zip_code)
+
+            # date
+            date = one_article.find('div', class_='aditem-main--top--right')
+            date = string_return_value(date)
+            
+            # picture (can be empty)
+            picture_div = one_article.find('div', class_='imagebox')
+            try:
+                image_url = picture_div['data-imgsrc']
+            except:
+                image_url = default_image_url
+            
+            item =  {
+                'id': item_id,
+                'heading': heading,
+                'description': info_text,
+                'price': price,
+                'date': date,
+                'image_url': image_url
+            }
+            
+            # append item to the returned list
+            list_with_items.append(item)
+            
+        #for debugging
+        print(url)
         # return list in json format
-        print(list_with_data)
-        return json.dumps(list_with_data)
+        return json.dumps(list_with_items)
 
     except:
         return json.dumps([])
-
-    
-#search term for testing
-get_search_entries("smartphone", "preis", 2, "privat", "gesuche", "", "")
 
